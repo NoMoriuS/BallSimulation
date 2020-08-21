4fc3ab65b3eba0888221a767411b40b50bcbc5bb
NoMore <vrodefedor@gmail.com>
2020-08-21 21:30:45 +0300

    Gravity, alpha bouncess

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,28 +4,26 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public bool useGravity;
+    public float gravityScale = 1;
     public Vector2 velocity;
     public float mouseImpulsePower;
-    public float r;
+    public float radius;
     float speed;
-    Wall[] walls;
+    Vector2 gravity;
 
     private void Start()
     {
-        walls = FindObjectsOfType<Wall>();
+        gravity = Physics2D.gravity;
     }
 
     private void FixedUpdate()
     {
         transform.position = Vector2.Lerp(transform.position, transform.position + new Vector3(velocity.x, velocity.y, 0), 1 * Time.fixedDeltaTime);
-        foreach (var item in walls)
-        {
-            if (IsTouchWall(item))
-            {
-                BounceOffWall(item);
-                return;
-            }
-        }
+        
+
+        if (useGravity)
+            velocity += gravity * gravityScale * Time.fixedDeltaTime;
     }
 
     private void Update()
@@ -37,28 +35,5 @@ public class Ball : MonoBehaviour
             moveVector.Normalize();
             velocity = moveVector * mouseImpulsePower;
         }
-    }
-
-    void BounceOffWall(Wall wall)
-    {
-        Debug.DrawLine(transform.position, transform.position + wall.transform.right, Color.green, 100);
-        velocity = Vector2.Reflect(velocity, wall.transform.right);
-        var cornersToPlayerDist = Vector2.Distance(wall.leftDownCorner, transform.position + r * wall.transform.right) + Vector2.Distance(wall.leftUpCorner, transform.position + r * wall.transform.right);
-        Debug.Log(cornersToPlayerDist);
-    }
-
-    bool IsTouchWall(Wall wall) 
-    {
-        Transform wallTrans = wall.transform;
-
-        Debug.DrawLine(wall.leftDownCorner, transform.position + r * wallTrans.right);
-        Debug.DrawLine(wall.leftUpCorner, transform.position + r * wallTrans.right);
-
-        var cornersToPlayerDist = Vector2.Distance(wall.leftDownCorner, transform.position + r * wallTrans.right) + Vector2.Distance(wall.leftUpCorner, transform.position + r * wallTrans.right);
-
-        if (Mathf.Abs(wall.size.y - cornersToPlayerDist) < 0.02f)
-            return true;
-        else
-            return false;
     }
 }
--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,21 +6,78 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     public Vector2 size;
-    public Vector3 leftUpCorner;
-    public Vector3 rightUpCorner;
-    public Vector3 leftDownCorner;
-    public Vector3 rightDownCorner;
+    public float bouncess = 0.4f;
+    public List<Ball> enteredBalls;
+    Vector3 topLimit;
+    Vector3 downLimit;
+    Vector3 leftUpCorner;
+    Vector3 rightUpCorner;
+    Vector3 leftDownCorner;
+    Vector3 rightDownCorner;
+    Ball[] balls;
+
+    private void Start()
+    {
+        balls = FindObjectsOfType<Ball>();
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var item in balls)
+        {
+            if (!enteredBalls.Contains(item))
+            {
+                if (IsTouchWall(item))
+                    OnBallEnter(item);
+            }
+            else
+            {
+                if (!IsTouchWall(item))
+                    enteredBalls.Remove(item);
+            }
+        }
+    }
 
     private void Update()
     {
-        leftUpCorner = transform.position + transform.up * size.y / 2f - transform.right * size.x / 2f;
-        rightUpCorner = transform.position + transform.up * size.y / 2f + transform.right * size.x / 2f;
-        leftDownCorner = transform.position - (transform.up * size.y / 2f) - transform.right * size.x / 2f;
-        rightDownCorner = transform.position - transform.up * size.y / 2f + transform.right * size.x / 2f;
+        topLimit = transform.position + transform.up * size.y / 2f;
+        downLimit = transform.position - transform.up * size.y / 2f;
+
+        leftUpCorner = topLimit - transform.right * size.x / 2f;
+        rightUpCorner = topLimit + transform.right * size.x / 2f;
+        leftDownCorner = downLimit - transform.right * size.x / 2f;
+        rightDownCorner = downLimit + transform.right * size.x / 2f;
 
         Debug.DrawLine(leftUpCorner, rightUpCorner, Color.green);
         Debug.DrawLine(rightUpCorner, rightDownCorner, Color.green);
         Debug.DrawLine(rightDownCorner, leftDownCorner, Color.green);
         Debug.DrawLine(leftDownCorner, leftUpCorner, Color.green);
+    }
+
+    void OnBallEnter(Ball ball)
+    {
+        enteredBalls.Add(ball);
+        BounceOffWall(ball);
+    }
+
+    void BounceOffWall(Ball ball)
+    {
+        Debug.DrawLine(ball.transform.position, ball.transform.position + transform.right, Color.green, 100000);
+        ball.velocity = Vector2.Reflect(ball.velocity, transform.right) * bouncess;
+    }
+
+    bool IsTouchWall(Ball ball)
+    {
+        Transform ballTrans = ball.transform;
+
+        Debug.DrawLine(topLimit, ballTrans.position + (ball.radius + size.x / 2f) * transform.right);
+        Debug.DrawLine(downLimit, ballTrans.position + (ball.radius + size.x / 2f) * transform.right);
+
+        var limitsToPlayerDist = Vector2.Distance(topLimit, ballTrans.position + (ball.radius + size.x / 2f) * transform.right) + Vector2.Distance(downLimit, ballTrans.position + (ball.radius + size.x / 2f) * transform.right);
+
+        if (Mathf.Abs(size.y - limitsToPlayerDist) < 0.01f)
+            return true;
+        else
+            return false;
     }
 }
