2e68170fe6f5cc4c134e94714a3424b60fa7338c
NoMore <vrodefedor@gmail.com>
2020-08-21 03:12:45 +0300

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Vector2 velocity;
+    public float mouseImpulsePower;
+    public float r;
+    float speed;
+    Wall[] walls;
+
+    private void Start()
+    {
+        walls = FindObjectsOfType<Wall>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector2.Lerp(transform.position, transform.position + new Vector3(velocity.x, velocity.y, 0), 1 * Time.fixedDeltaTime);
+        foreach (var item in walls)
+        {
+            if (IsTouchWall(item))
+            {
+                BounceOffWall(item);
+                return;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 moveVector = mousePos - transform.position;
+            moveVector.Normalize();
+            velocity = moveVector * mouseImpulsePower;
+        }
+    }
+
+    void BounceOffWall(Wall wall)
+    {
+        Debug.DrawLine(transform.position, transform.position + wall.transform.right, Color.green, 100);
+        velocity = Vector2.Reflect(velocity, wall.transform.right);
+        var cornersToPlayerDist = Vector2.Distance(wall.leftDownCorner, transform.position + r * wall.transform.right) + Vector2.Distance(wall.leftUpCorner, transform.position + r * wall.transform.right);
+        Debug.Log(cornersToPlayerDist);
+    }
+
+    bool IsTouchWall(Wall wall) 
+    {
+        Transform wallTrans = wall.transform;
+
+        Debug.DrawLine(wall.leftDownCorner, transform.position + r * wallTrans.right);
+        Debug.DrawLine(wall.leftUpCorner, transform.position + r * wallTrans.right);
+
+        var cornersToPlayerDist = Vector2.Distance(wall.leftDownCorner, transform.position + r * wallTrans.right) + Vector2.Distance(wall.leftUpCorner, transform.position + r * wallTrans.right);
+
+        if (Mathf.Abs(wall.size.y - cornersToPlayerDist) < 0.02f)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Wall : MonoBehaviour
+{
+    public Vector2 size;
+    public Vector3 leftUpCorner;
+    public Vector3 rightUpCorner;
+    public Vector3 leftDownCorner;
+    public Vector3 rightDownCorner;
+
+    private void Update()
+    {
+        leftUpCorner = transform.position + transform.up * size.y / 2f - transform.right * size.x / 2f;
+        rightUpCorner = transform.position + transform.up * size.y / 2f + transform.right * size.x / 2f;
+        leftDownCorner = transform.position - (transform.up * size.y / 2f) - transform.right * size.x / 2f;
+        rightDownCorner = transform.position - transform.up * size.y / 2f + transform.right * size.x / 2f;
+
+        Debug.DrawLine(leftUpCorner, rightUpCorner, Color.green);
+        Debug.DrawLine(rightUpCorner, rightDownCorner, Color.green);
+        Debug.DrawLine(rightDownCorner, leftDownCorner, Color.green);
+        Debug.DrawLine(leftDownCorner, leftUpCorner, Color.green);
+    }
+}
